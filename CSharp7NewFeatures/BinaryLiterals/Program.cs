cce637b59e9cb62be3033f0939080701dda4103e
ilkayilknur <ilkay@ilkayilknur.com>
2016-07-09 12:53:44 +0000

    initial solution creations and binery literals sample implementation.

---
+++ CSharp7NewFeatures/BinaryLiterals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryLiterals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0b110;
+            int y = 0b1000100;
+
+            Console.WriteLine($"X={x}, Y={y}");
+
+            Permissions permission = Permissions.Read | Permissions.Write;
+            Console.WriteLine(permission);
+        }
+    }
+
+    //Using binary literals with flag enums.
+    [Flags]
+    enum Permissions
+    {
+        Create = 0b1,
+        Read = 0b10,
+        Write = 0b100,
+        Delete = 0b1000,
+    }
+}
